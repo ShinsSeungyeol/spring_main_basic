ffb6ace9c0a64c9154044ef19588f18fd99df382
신승열 <shinseungyeol0227@gmail.com>
2023-12-22 11:19:13 +0900

    init commit

---
+++ src/main/java/com/example/spring_main_basic/SpringMainBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_main_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMainBasicApplication.class, args);
+    }
+
+}
