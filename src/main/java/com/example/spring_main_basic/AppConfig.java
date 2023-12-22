22236c0526458e1cd7e4564ce42bcf9fbc50f821
신승열 <shinseungyeol0227@gmail.com>
2023-12-22 14:26:56 +0900

    AppConfig 클래스를 생성하여 DI를 맡김. -> 관심사를 분리함

---
+++ src/main/java/com/example/spring_main_basic/AppConfig.java
@@ -0,0 +1,23 @@
+package com.example.spring_main_basic;
+
+import com.example.spring_main_basic.discount.FixDiscountPolicy;
+import com.example.spring_main_basic.member.MemberService;
+import com.example.spring_main_basic.member.MemberServiceImpl;
+import com.example.spring_main_basic.member.MemoryMemberRepository;
+import com.example.spring_main_basic.order.OrderService;
+import com.example.spring_main_basic.order.OrderServiceImpl;
+
+/**
+ * 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+ * 생성자를 통해서 주입 해준다.
+ * 의존관계에 대한 고민은 외부에 맡기고 실행에만 집중한다.
+ */
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
--- src/test/java/com/example/spring_main_basic/order/OrderServiceTest.java
+++ src/test/java/com/example/spring_main_basic/order/OrderServiceTest.java
@@ -1,16 +1,24 @@
 package com.example.spring_main_basic.order;
 
+import com.example.spring_main_basic.AppConfig;
 import com.example.spring_main_basic.member.Grade;
 import com.example.spring_main_basic.member.Member;
 import com.example.spring_main_basic.member.MemberService;
 import com.example.spring_main_basic.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
 
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
     @Test
     void createOrder() {
         Long memberId = 1L;
