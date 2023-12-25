1980d16e3232b5194eec46c1759f633fad81fb04
신승열 <shinseungyeol0227@gmail.com>
2023-12-25 16:12:26 +0900

    빈 라이프사이클 초기화, 소멸 인터페이스를 구현하여 해결
    1. InitializingBean
    2. DisposableBean
    
    현재는 잘 사용하지 않음

---
+++ src/test/java/com/example/spring_main_basic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package com.example.spring_main_basic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        // 클로즈할 수 있는 ConfigurableApplicationContext
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+
+            networkClient.setUrl("http://localhost:4040");
+            return networkClient;
+        }
+    }
+}
